03733f73e91d0d20b24ef400910e6c9759300e04
ritairmayani <ritairmayani4@gmail.com>
2024-09-20 22:57:25 +0700

    perubahan 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,6 +38,10 @@
                     </svg>
                 </div>
 
+                <div class="mt-6 text-xl font-semibold text-gray-900 dark:text-white flex justify-center">
+                    <h1 style="font-size: 3rem;">Rita Irma Yani- 2217051047</h1>
+                </div>
+
                 <div class="mt-16">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                         <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
